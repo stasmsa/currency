902c65b3da253f0df17b79338ee4fe77c3fd160c
Stas Malenkov <stanislav.malenkov@instafxgroup.com>
2017-07-12 13:31:38 +0200

    init

---
+++ app/index.js
@@ -0,0 +1,16 @@
+// @flow
+import React from 'react';
+import EStyleSheet from 'react-native-extended-stylesheet';
+
+import Home from './screens/Home';
+
+EStyleSheet.build({
+  $primaryBlue: '#4f6d7a',
+
+  $white: '#fff',
+  $border: '#e2e2e2',
+  $inputText: '#797979',
+  $lightGray: '#f0f0f0',
+});
+
+export default () => <Home />;
---
+++ app/screens/Home.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { StatusBar } from 'react-native';
+
+import { Container } from '../components/Container';
+import { Logo } from '../components/Logo';
+import { InputWithButton } from '../components/TextInput';
+
+const TEMP_BASE_CURRENCY = 'USD';
+const TEMP_QUOTE_CURRENCY = 'GBP';
+const TEMP_BASE_PRICE = '100';
+const TEMP_QUOTE_PRICE = '79.74';
+
+class Home extends Component {
+  handlePressBaseCurrency = () => {
+    console.log('press base');
+  }
+
+  handlePressQuoteCurrency = () => {
+    console.log('press quote');
+  }
+
+  handleTextChange = (text) => {
+    console.log('change text', text);
+  }
+
+  render() {
+    return (
+      <Container>
+        <StatusBar backgroundColor="#4f6d7a" barStyle="light-content" />
+        <Logo />
+        <InputWithButton
+          buttonText={TEMP_BASE_CURRENCY}
+          onPress={this.handlePressBaseCurrency}
+          defaultValue={TEMP_BASE_PRICE}
+          keyboardType="numeric"
+          onChangeText={this.handleTextChange}
+        />
+        <InputWithButton
+          buttonText={TEMP_QUOTE_CURRENCY}
+          editable={false}
+          onPress={this.handlePressQuoteCurrency}
+          value={TEMP_QUOTE_PRICE}
+        />
+      </Container>
+    );
+  }
+}
+
+export default Home;
