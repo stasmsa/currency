60e6e08677d448524c7652fca706cba1c8a594a6
Stas Malenkov <stanislav.malenkov@instafxgroup.com>
2017-07-13 14:10:29 +0200

    List currencies

--- app/index.js
+++ app/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import EStyleSheet from 'react-native-extended-stylesheet';
 
 import Home from './screens/Home';
+import CurrencyList from './screens/CurrencyList';
 
 EStyleSheet.build({
   $primaryBlue: '#4f6d7a',
@@ -11,6 +12,7 @@ EStyleSheet.build({
   $border: '#e2e2e2',
   $inputText: '#797979',
   $lightGray: '#f0f0f0',
+  $darkText: '#343434',
 });
 
-export default () => <Home />;
+export default () => <CurrencyList />;
