dd2ed8d80aac8cd64f281920889c79aea60bdc4a
Stas Malenkov <stanislav.malenkov@instafxgroup.com>
2017-07-14 09:27:51 +0200

    Navigation to currency list

--- app/components/Header/styles.js
+++ app/components/Header/styles.js
@@ -1,5 +1,4 @@
 import EStyleSheet from 'react-native-extended-stylesheet';
-import { StatusBar } from 'react-native';
 
 const styles = EStyleSheet.create({
   container: {
@@ -9,10 +8,6 @@ const styles = EStyleSheet.create({
     top: 0,
     '@media ios': {
       paddingTop: 20,
-    },
-    '@media android': {
-      paddingTop: 0,
-      //paddingTop: StatusBar.currentHeight,
     },
   },
   button: {
